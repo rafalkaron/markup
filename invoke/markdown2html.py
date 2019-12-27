9eb5ae1de218ddb9192e645ccddeb319c0a6b175
rafalkaron <rafalkaron@gmail.com>
2019-12-27 15:39:25 +0100

    you can now select a markdown file or provide your ow markdown syntax

--- invoke/markdown2html.py
+++ invoke/markdown2html.py
@@ -1,8 +1,9 @@
-__version__ = "0.1"
+# coding: utf-8
+__version__ = "0.2"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
 """
-    markdown2html (Codename: Vain Flamingo)
+    markdown2html (Codename: Asymetric Flamingo)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     Convert Markdown to HTML5.
@@ -16,14 +17,44 @@ import datetime
 _md = mistune.Markdown()
 _timestamp = datetime.datetime.now()
 
+
 def intro():
     print("Convert your Markdown files to HTML5.")
-intro()
+
+def feed():
+    _input_type = input("Do you want to select a Markdown file? [Y/N]")
+    if _input_type == "Y":
+        load_file()
+        exit()
+    elif _input_type =="y":
+        load_file()
+        exit()
+    if _input_type =="N":
+        new_file()
+        exit()
+    elif _input_type =="n":
+        new_file()
+        exit()
+    else:
+        print("Try answering the following question again by entering the \"Y\" or \"N\" characters withour quotation marks.")
+        feed()
+
+def new_file():
+    _input_text = input("Enter Markdown syntax: ")
+    out()
+    _out_file.write(_md(_input_text))
+    exit()
+
+def load_file():
+    _file_location = input("Provide a full path to the Markdown file: ")
+    _input_file = open(_file_location)
+    out()
+    _out_file.write(_md(_input_file.read()))
 
 def out():
-#Function Variables
-    _input_text = input("Enter Markdown syntax: ")
+    global _out_file
     _out_file = open("out_" +str(_timestamp.strftime("%d_%m_%y-%H-%M-%S")) + ".html", "w")
-#Invocations
-    _out_file.write(_md(_input_text)) #creates HTML5 from the MD syntax that you entered.
-out()
+    
+#Function calls
+intro()
+feed()
