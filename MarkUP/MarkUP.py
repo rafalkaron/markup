51465170d19ad4d3eb3b505334a888771cb617a7
rafalkaron <rafalkaron@gmail.com>
2019-12-30 13:29:54 +0100

    Added DITA conversion support

--- MarkUP/MarkUP.py
+++ MarkUP/MarkUP.py
@@ -1,9 +1,9 @@
 # coding: utf-8
-__version__ = "0.6"
+__version__ = "0.7"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
 """
-    MarkUP (Codename: Collected Broccoli)
+    MarkUP (Codename: Structured Broccoli)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     Convert Markdown to HTML5 or DITA.
@@ -14,7 +14,6 @@ __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
     Upcoming features:
     - Multiline input support
-    - Conversion to DITA
     - CLI
     - GUI
 """
@@ -24,6 +23,7 @@ import mistune              #Markdown parser
 import datetime             #Date and time
 import os                   #OS interface to work with files on a lower level
 from pathlib import Path    #Allows for Path subclass
+import shutil               #Needed to remove files in the tmp folder
 
 #Global Variables
 _md = mistune.Markdown()
@@ -32,6 +32,8 @@ _out_folder = Path("out")
 _out_folder_str = str(_out_folder)    
 _src_folder = Path("src")
 _src_folder_str = str(_src_folder)
+_tmp_folder = Path("tmp")
+_tmp_folder_str = str(_tmp_folder)
 
 #Functions
 def intro():
@@ -43,6 +45,10 @@ def feed():
         new_md_to_html5()
     elif _input_type == "Y" or _input_type =="y":
         convert_md_to_html5()
+    elif _input_type == "md2dita":
+        convert_md_to_dita()
+    elif _input_type == "newmd2dita":
+        new_md_to_dita()
     else: 
         print("Try answering the following question again by entering the \"Y\" or \"N\" characters withour quotation marks.")
         feed()
@@ -57,7 +63,7 @@ def multiline():
         MultiLine.append(line)
     else:
         break
-    _markdown_syntax = '\n'.join(MultiLine)
+    _md_input = '\n'.join(MultiLine)
     """
 
 def convert_md_to_html5():
@@ -65,28 +71,47 @@ def convert_md_to_html5():
     _convert_md_to_html5_called = True #checks if the function was run
     _file_location = input("Provide a full path to the Markdown file: ")
     _input_file = open(_file_location)
-    out_file()
-    _out_file.write(_md(_input_file.read()))
-    _out_file.close
+    out_html5()
+    _out_html5.write(_md(_input_file.read()))
+    _out_html5.close
 _convert_md_to_html5_called = False #checks if the function was not run
+
+def convert_md_to_dita():
+    global _convert_md_to_dita_called
+    _convert_md_to_dita_called = True
+    _file_location = input("Provide a full path to the Markdown file: ")
+    _md_to_dita = os.system("markdown2dita -i " + _file_location + " -o " + _out_folder_str + "/" + "out_" +str(_timestamp.strftime("%d_%m_%y-%H-%M-%S")) + ".dita")
+_new_md_to_dita_called = False
 
 def new_md_to_html5():
     global _new_md_to_html5_called
     _new_md_to_html5_called = True #checks if the function was run
-    global _markdown_syntax
-    _markdown_syntax = input("Enter Markdown syntax: ")
-    out_file()
-    _out_file.write(_md(_markdown_syntax))
-    _out_file.close
+    md_input()
+    out_html5()
+    _out_html5.write(_md(_md_input))
+    _out_html5.close
     src_file()
-_new_md_to_html5_called= False #checks if the function was not run
+_new_md_to_html5_called = False #checks if the function was not run
 
-"""
 def new_md_to_dita():
+    global _new_md_to_dita_called
+    _new_md_to_dita_called = True
+    tmp_folder()
+    md_input()
+    _tmp_file = open(_tmp_folder_str + "/" + "tmp.md", "w")
+    _tmp_file.write(_md(_md_input))
+    _md_to_dita = os.system("markdown2dita -i " + _tmp_folder_str + "/" + "tmp.md" + " -o " + _out_folder_str + "/" + "out_" +str(_timestamp.strftime("%d_%m_%y-%H-%M-%S")) + ".dita")
+    src_file()
+_new_md_to_dita_called = False
 
+def tmp_folder():
+    if not os.path.exists(_tmp_folder):
+        os.mkdir(_tmp_folder)    
 
-def convert_md_to_dita():
-"""
+def tmp_folder_del():
+    if os.path.exists(_tmp_folder):
+        shutil.rmtree(_tmp_folder)
+
 def src_file():
     #Determines if you want to save the source file
     _save_source = input("Do you want to retain the Markdown source file? \n [Y] - Saves the Markdown file in the src folder. \n [N] - Discards the Markdown file. \n Enter [Y/N] ")
@@ -95,20 +120,24 @@ def src_file():
     elif _save_source == "Y" or _save_source =="y":
         global _src_file
         _src_file = open(_src_folder_str + "/" + "src_" +str(_timestamp.strftime("%d_%m_%y-%H-%M-%S")) + ".md", "w")
-        _src_file.write(_markdown_syntax)
+        _src_file.write(_md_input)
         _src_file.close
         print("The source Markdown file will be saved in the " + _src_folder_str + " directory.")
     else: 
         print("Try answering the following question again by entering the \"Y\" or \"N\" characters withour quotation marks.")
         src_file()
 
+def md_input():
+    global _md_input
+    _md_input = input("Enter Markdown: ")
+
 def src_folder():
     if not os.path.exists(_src_folder):         #If the directory does not exist...
         os.mkdir(_src_folder)                   #Create the "src" directory in the script directory
 
-def out_file():
-    global _out_file
-    _out_file = open(_out_folder_str + "/" + "out_" +str(_timestamp.strftime("%d_%m_%y-%H-%M-%S")) + ".html", "w")
+def out_html5():
+    global _out_html5
+    _out_html5 = open(_out_folder_str + "/" + "out_" +str(_timestamp.strftime("%d_%m_%y-%H-%M-%S")) + ".html", "w")
 
 def out_folder():
     if not os.path.exists(_out_folder):         #If the directory does not exist...
@@ -127,4 +156,5 @@ out_folder()
 src_folder()
 feed()
 summary()
+tmp_folder_del()
 exit()
