9122628cb45a1c141cbadae62868c708821abb29
rafalkaron <rafalkaron@gmail.com>
2020-01-27 15:44:55 +0100

    Added colors

--- MarkUP/MarkUP.py
+++ MarkUP/MarkUP.py
@@ -17,34 +17,48 @@
     - Log fixes (consecutive numbers)
     - The <title> tag should encapsulate the first #, the <abstract> tag should encapsulate what's under the first #
     - Console should count-down to exit
-    - pretty-printing parser does not support MARKDOWN and markdown extensions
+    - Program crashes if there's a file with markdown extension that actually contains the XML code (cannot be processed by the parser) - test out if that happens with codeblocks
+    - Additional ">" characher after conbody is sometimes inserted
+    - Set read-only to input files?
+    - Error msg if sth goes wrong
 """
 from __future__ import print_function
-import argparse, sys, mistune, os, glob, time, re, random, string, datetime, xml.dom.minidom
+import argparse, sys, mistune, os, glob, time, re, random, string, datetime
+from xml.dom.minidom import parseString, xml
 
 __version__ = "1.1"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 
-# MarkUP Lite Variables
-## Markup Lite executable location
+# MarkUP Variables
+## Markup executable location
 _markup_filepath = os.path.abspath(__file__)
 _markup_filename = os.path.basename(__file__)
 _markup_directory = _markup_filepath.replace(_markup_filename, "").replace("\\", "/")
-## Markdown files in the Markup Lite location with different extensions
+## Markdown files in the Markup location with different extensions
 _md_files = glob.glob(_markup_directory + "/*.md")
 _md_files_upper = glob.glob(_markup_directory + "/*.MD")
 _markdown_files = glob.glob(_markup_directory + "/*.markdown")
 _markdown_files_upper = glob.glob(_markup_directory + "/*.MARKDOWN")
-### Aggregation of every Markup file in the Markup Lite location
+### Aggregation of every Markup file in the Markup location
 _markdown_files_all = list(set(_md_files + _md_files_upper + _markdown_files + _markdown_files_upper))
-## Converted DITA files
-_dita_files = glob.glob(_markup_directory + "/*.dita")
-# Timestamps
+# Timestamps for logs
 _timestamp = datetime.datetime.now()
-# MarkUP Lite Code
 
+class termcolor:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+# MarkUP Code
 def intro():
-    print("Converting your Markdown files to DITA...")
+    print(termcolor.GREEN + "Converting the following Markdown files from " + termcolor.UNDERLINE + _markup_directory + termcolor.END + termcolor.GREEN + " to DITA" + termcolor.END)
 
 def convert():
     for _markdown_file in _markdown_files_all:
@@ -52,6 +66,7 @@ def convert():
         global _topic_id
         _topic_id = "topic_" + "".join([random.choice(string.ascii_lowercase + string.digits) for n in range(8)])
         _input_str = open(_markdown_file, 'r').read()
+        
         class Markdown(mistune.Markdown):
             def __init__(self, renderer=None, inline=None, block=None, **kwargs):
                 if not renderer:
@@ -62,7 +77,7 @@ def convert():
                     renderer=renderer, inline=inline, block=block)
 
             def parse(self, text, page_id = _topic_id,
-                    title= _file_title.strip(_markup_directory)):  # The first header as a title?
+                    title= _file_title.replace(_markup_directory, "")):
                 output = super(Markdown, self).parse(text)
                 if output.startswith('</section>'):
                     output = output[9:]
@@ -110,13 +125,13 @@ def convert():
         _dita_output_prettier = '\n'.join(list(filter(lambda x: len(x.strip()), _dita_output_pretty.split('\n'))))
         with open(re.sub(r"(\.md|\.markdown)", ".dita", _markdown_file, flags=re.IGNORECASE), "w") as output_file:
             output_file.write(_dita_output_prettier)
-            
+
         # Logs
         global _log_converted
         _log_converted = (_markdown_file + " -> " + re.sub(r"(\.md|\.markdown)", ".dita", _markdown_file, flags=re.IGNORECASE)).replace("\\", "/")
         global _log_topic_id
         _log_topic_id =  _topic_id
-        print(_log_converted + " [@ID=" + _log_topic_id +"]")
+        print(" - " + _log_converted.replace(_markup_directory, "") + " [@ID=" + _log_topic_id +"]")
         if _log_called == True:
             log_items()        
 
@@ -137,14 +152,20 @@ def log_items():
         log_file.write("[%s] " %i + _log_converted + " [@ID=" + _log_topic_id +"]" + "\n")
 
 def summary():
-    print("Conversion successful!\nThis window will close automatically in 5 seconds.")
-    time.sleep(5)
+    if _log_called == False:
+        print(termcolor.GREEN + "Conversion successful!" + termcolor.END) 
+    if _log_called == True:
+        print(termcolor.GREEN + "Conversion successful! For detailed information, see log_markup.txt" + termcolor.END)
+    _exit_prompt = input(termcolor.BOLD + termcolor.YELLOW + "To exit, press [Enter]" + termcolor.END)
+    if _exit_prompt:
+        exit(0)
 
 # markdown2dita code
 class Renderer(mistune.Renderer):
     for _markdown_file in _markdown_files_all:
         global _topic_id
         _topic_id = "\"topic_" + "".join([random.choice(string.ascii_lowercase + string.digits) for n in range(8)]) + "\""
+    
     def codespan(self, text):
         return '<codeph>{0}</codeph>'.format(escape(text.rstrip()))
 
@@ -163,7 +184,6 @@ class Renderer(mistune.Renderer):
         return '<codeblock>{0}</codeblock>'.format(text)
 
     def header(self, text, level, raw=None):
-        #  Dita only supports one title per section
         title_level = self.options.get('title_level', 2)
         if level <= title_level:
             return '</section><section><title>{0}</title>'.format(text)
@@ -177,12 +197,9 @@ class Renderer(mistune.Renderer):
         return '<i>{0}</i>'.format(text)
 
     def hrule(self):
-        # Dita has no horizontal rule, ignore it
-        # could maybe divide sections?
         return ''
 
     def inline_html(self, text):
-        # Dita does not support inline html, just pass it through
         return text
 
     def list_item(self, text):
@@ -195,9 +212,7 @@ class Renderer(mistune.Renderer):
             return '<ul>{0}</ul>'.format(body)
 
     def image(self, src, title, text):
-
         # Derived from the mistune library source code
-
         src = mistune.escape_link(src)
         text = escape(text, quote=True)
         if title:
@@ -207,7 +222,6 @@ class Renderer(mistune.Renderer):
                       .format(title, src, text))
         else:
             output = '<image href="{0}" alt="{1}"/>'.format(src, text)
-
         return output
 
     def table(self, header, body, cols):
@@ -215,7 +229,6 @@ class Renderer(mistune.Renderer):
                       for x in range(cols)]
         output_str = ('<table>\n<tgroup cols="{0}">\n{1}\n'
                       .format(cols, '\n'.join(col_string)))
-
         return (output_str + '<thead>\n' + header + '</thead>\n<tbody>\n' +
                 body + '</tbody>\n</tgroup>\n</table>')
 
@@ -224,37 +237,31 @@ class Renderer(mistune.Renderer):
 
     def table_cell(self, content, **flags):
         align = flags['align']
-
         if align:
             return '<entry align="{0}">{1}</entry>\n'.format(align, content)
         else:
             return '<entry>{0}</entry>\n'.format(content)
-
+    
     def autolink(self, link, is_email=False):
         text = link = escape(link)
         if is_email:
             link = 'mailto:{0}'.format(link)
         return '<xref href="{0}">{1}</xref>'.format(link, text)
-
+    
     def footnote_ref(self, key, index):
         return ''
-
+    
     def footnote_item(self, key, text):
         return ''
-
     def footnotes(self, text):
         return ''
-
+    
     def strikethrough(self, text):
         return text
-
-
 
 # markdown2dita code
 def escape(text, quote=False, smart_amp=True):
     return mistune.escape(text, quote=quote, smart_amp=smart_amp)
-
-
 
 # Invocations
 intro()
