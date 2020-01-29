6594c3b408dfb6dd90aa4abfa3c09894d3219754
rafalkaron <rafalkaron@gmail.com>
2020-01-29 16:19:55 +0100

    Added an exception that doesn't run parser if there's a problem with it

--- MarkUP/MarkUP.py
+++ MarkUP/MarkUP.py
@@ -120,11 +120,14 @@ def convert():
         # Output
         _markdown = Markdown()
         _dita_output = _markdown(_input_str)
-        _dita_output_parse = xml.dom.minidom.parseString(_dita_output)
-        _dita_output_pretty = _dita_output_parse.toprettyxml(indent="\t", newl="\n")
-        _dita_output_prettier = '\n'.join(list(filter(lambda x: len(x.strip()), _dita_output_pretty.split('\n'))))
         with open(re.sub(r"(\.md|\.markdown)", ".dita", _markdown_file, flags=re.IGNORECASE), "w") as output_file:
-            output_file.write(_dita_output_prettier)
+            try:
+                _dita_output_parse = xml.dom.minidom.parseString(_dita_output)
+                _dita_output_pretty = _dita_output_parse.toprettyxml(indent="\t", newl="\n")
+                _dita_output_prettier = '\n'.join(list(filter(lambda x: len(x.strip()), _dita_output_pretty.split('\n'))))
+                output_file.write(_dita_output_prettier)
+            except xml.parsers.expat.ExpatError:
+                output_file.write(_dita_output)
 
         # Logs
         global _log_converted
