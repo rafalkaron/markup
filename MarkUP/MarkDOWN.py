8935c37821fe851b7e7f179a1d50a833dad43b8a
rafalkaron <rafalkaron@gmail.com>
2020-02-12 16:01:26 +0100

    MarkDOWN

---
+++ MarkUP/MarkDOWN.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+"""
+    MarkDOWN (Codename: )
+    ****************************************************************
+
+    Convert all HTML files to MD
+    
+    ****************************************************************    
+"""
+import argparse, sys, mistune, os, glob, re, string
+import tomd
+
+__version__ = "0.1"
+__author__ = "Rafał Karoń <rafalkaron@gmail.com>"
+
+if os.name=="nt":
+    _script_filepath = os.path.abspath(__file__)
+    _script_filename = os.path.basename(__file__)
+    _script_directory = _script_filepath.replace(_script_filename, "").replace("\\", "/")
+    _html_files = glob.glob(_script_directory + "/*.html")
+    _html_files_upper = glob.glob(_script_directory + "/*.HTML")
+    
+if os.name=="posix":
+    _script_directory = input("Enter a full path to the directory that contains the HTML files that you want to convert: ").replace("\\", "/").replace("//", "/")
+    _md_files = glob.glob(_script_directory + "/*.html")
+    _md_files_upper = glob.glob(_script_directory + "/*.HTML")
+
+_html_files_all = list(set(_html_files + _html_files_upper))
+
+def html_to_dita():
+    for _html_file in _html_files_all:
+    #Variables
+        _in_file_path = _html_file.replace("\\", "/")
+        _in_file_name = _in_file_path.replace(_script_directory + "/", "").replace(_script_directory, "")
+        _out_file_path = re.sub(r".html", ".md", _in_file_path, flags=re.IGNORECASE)
+        _out_file_name = _out_file_path.replace(_script_directory + "/", "").replace(_script_directory, "")
+    #Input
+        _input_str = open(_html_file, 'r').read()
+    #Conversion
+        _tomd = tomd.convert(_input_str)
+    #Output
+        with open(_out_file_path, "w") as output_file:
+            output_file.write(_tomd)
+
+
+
+html_to_dita()
---
+++ MarkUP/PatricksSample.md
@@ -0,0 +1,7 @@
+
+The Help Center is made up of two parts: a knowledge base and a separate community platform. The community consists of questions and answers organized by topic. Questions can include ideas, tips, or any other community item. To get started, see [Managing community content](https://support.zendesk.com/hc/en-us/articles/203664406).
+
+
+**Note**: Don't confuse topics with articles. In the community, topics are top-level containers for questions.
+
+A key ingredient to a successful community are the moderators. Recruit knowledgeable users who are eager to share their product knowledge, lead lost users to safety, and pick up the latest tricks of the trade from other product experts. 
