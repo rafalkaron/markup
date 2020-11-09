af44beb54d508cc11132228a8762618b2fed8d73
rafalkaron <rafalkaron@gmail.com>
2020-11-10 00:18:46 +0100

    fixed output folder for single files

--- __main__.py
+++ __main__.py
@@ -13,7 +13,7 @@ from MarkUP import (md_dita,
 __version__ = "0.4"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 def main():
-    # sys.tracebacklimit = 0 # Disable traceback messages
+    #sys.tracebacklimit = 0 # Disable traceback messages
     par = argparse.ArgumentParser(description="Batch-convert Markdown and HTML files.", formatter_class=argparse.RawTextHelpFormatter)
     par.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
     par.add_argument("input", type=str, help="provide a filepath to a file or a folder with files that you want to convert")
@@ -29,17 +29,24 @@ def main():
     if args.output:
         output_dir = args.output
     elif not args.output:
-        output_dir = args.input
+        if os.path.isfile:
+            output_dir = os.path.dirname(args.input)
+        elif os.path.isdir:
+            output_dir = args.input
+
+    # Make paths uniform for every platform
+    args.input = args.input.replace("\\", "/")
+    output_dir = output_dir.replace("\\", "/")
 
     # Conversion types
     if args.convert == "md_html":
-        md_html(args.input.replace("\\", "/"), output_dir.replace("\\", "/"))
+        md_html(args.input, output_dir)
     elif args.convert == "md_dita":
-        md_dita(args.input.replace("\\", "/"), output_dir.replace("\\", "/"))
+        md_dita(args.input, output_dir)
     elif args.convert == "html_dita":
-        html_dita(args.input.replace("\\", "/"), output_dir.replace("\\", "/"))
+        html_dita(args.input, output_dir)
     elif args.convert == "html_md":
-        html_md(args.input.replace("\\", "/"), output_dir.replace("\\", "/"))
+        html_md(args.input, output_dir)
     else:
         print(" [!] Run MarkUP again and set a right conversion type.\n")
         par.print_help()
