2d269291b3a4945fad121dcd6613b4c52cf271b1
rafalkaron <rafalkaron@gmail.com>
2020-11-09 10:11:21 +0100

    code cleanup

--- MarkUP/convert.py
+++ MarkUP/convert.py
@@ -43,41 +43,41 @@ def html_str_to_markdown_str(html_str):
     markdown_str = re.sub(r"\n\s*\n\s*", "\n\n", markdown_str)
     return markdown_str
 
-def convert_folder(source, input_extension, converter, output_folder, output_extension):
+def convert_folder(source, source_extension, converter, output_dir, output_extension):
     """Convert files in a folder."""
     start_time = time.time()
-    input_files_list = files_list(source, input_extension)
+    input_files_list = files_list(source, source_extension)
     files_number = len(input_files_list)
     if files_number == 0:
-        raise Exception(f"No {input_extension.upper()} files found in {source}")
+        raise Exception(f"No {source_extension.upper()} files found in {source}")
     part = 100 / files_number
     progress = 0
     pb(progress)
     for input_filepath in input_files_list:
         progress += part
         pb(progress)
-        output_file = os.path.basename(re.sub(f".{input_extension}", f".{output_extension}", input_filepath, flags=re.IGNORECASE))
+        output_file = os.path.basename(re.sub(f".{source_extension}", f".{output_extension}", input_filepath, flags=re.IGNORECASE))
         output_str = converter(read_file(input_filepath), output_file)
-        output_filepath = output_folder + "/" + output_file
+        output_filepath = output_dir + "/" + output_file
         if os.path.isfile(output_filepath):
             prompt = input(f" [?] Do you want to overwrite {output_filepath}? [y/n]: ")
             if prompt == "y" or prompt == "Y":
                 save_str_as_file(output_str,output_filepath)
                 elapsed_time = time.time() - start_time
-                print(f"Converted {files_number} {input_extension.upper()} file(s) to {output_extension.upper()} in {round(elapsed_time, 3)} seconds.")
+                print(f"Converted {files_number} {source_extension.upper()} file(s) to {output_extension.upper()} in {round(elapsed_time, 3)} seconds.")
             elif prompt != "y" or prompt != "Y":
                 print("Aborted process.")
 
-def convert_file(source, input_extension, converter, output_extension):
+def convert_file(source, source_extension, converter, output_extension):
     """Convert a specific file."""
     start_time = time.time()
     pb(0)
     source_extension = file_extension(source)
-    if input_extension.lower() != source_extension.lower():
-        raise Exception(f"You selected a wrong file type. Please select a(n) {input_extension.upper()} file.")
-    output_file = os.path.basename(re.sub(f".{input_extension}", f".{output_extension}", source, flags=re.IGNORECASE))
-    output_folder = os.path.dirname(os.path.abspath(source))
-    output_filepath = output_folder + "/" + output_file
+    if source_extension.lower() != source_extension.lower():
+        raise Exception(f"You selected a wrong file type. Please select a(n) {source_extension.upper()} file.")
+    output_file = os.path.basename(re.sub(f".{source_extension}", f".{output_extension}", source, flags=re.IGNORECASE))
+    output_dir = os.path.dirname(os.path.abspath(source))
+    output_filepath = output_dir + "/" + output_file
     if os.path.isfile(output_filepath):
         prompt = input(f" [?] Do you want to overwrite {output_filepath}? [y/n]: ")
         if prompt == "y" or prompt == "Y":
@@ -85,7 +85,39 @@ def convert_file(source, input_extension, converter, output_extension):
             save_str_as_file(output_str, output_filepath)
             elapsed_time = time.time() - start_time
             pb(100)
-            print(f"Converted one {input_extension.upper()} file to {output_extension.upper()} in {round(elapsed_time, 3)} seconds.")
+            print(f"Converted one {source_extension.upper()} file to {output_extension.upper()} in {round(elapsed_time, 3)} seconds.")
         elif prompt != "y" or prompt != "Y":
             print("Aborted process.")
             return False
+
+def md_html(source, output_dir):
+    if os.path.isfile(source):
+        convert_file(source, "md", markdown_str_to_html_str, "html")
+    elif os.path.isdir(source):
+        convert_folder(source, "md", markdown_str_to_html_str, output_dir, "html")
+    else:
+        raise Exception(f" [!] {source} does not exist.")
+
+def md_dita(source, output_dir):
+    if os.path.isfile(source):
+        convert_file(source, "md", markdown_str_to_dita_str, "dita")
+    elif os.path.isdir(source):
+        convert_folder(source, "md", markdown_str_to_dita_str, output_dir, "dita")
+    else:
+        raise Exception(f" [!] {source} does not exist.")
+
+def html_dita(source, output_dir):
+    if os.path.isfile(source):
+        convert_file(source, "html", html_str_to_dita_str, "dita")
+    elif os.path.isdir(source):
+        convert_folder(source, "html", html_str_to_dita_str, output_dir, "dita")
+    else:
+        raise Exception(f" [!] {source} does not exist.")
+
+def html_md(source, output_dir):
+    if os.path.isfile(source):
+        convert_file(source, "html", html_str_to_markdown_str, "md")
+    elif os.path.isdir(source):
+        convert_folder(source, "html", html_str_to_markdown_str, output_dir, "md")
+    else:
+        raise Exception(f" [!] {source} does not exist.")
--- __main__.py
+++ __main__.py
@@ -7,13 +7,10 @@ import os
 import sys
 import argparse
 from MarkUP import (progressbar as pb,
-                    convert_file,
-                    convert_folder,
-                    markdown_str_to_html_str,
-                    html_str_to_markdown_str,
-                    markdown_str_to_dita_str,
-                    html_str_to_dita_str
-                    )
+                    md_dita,
+                    md_html,
+                    html_md,
+                    html_dita)
 __version__ = "0.3"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 
@@ -24,50 +21,27 @@ def main():
     par.add_argument("convert", type=str, help="""set the conversion type:
  * md_html - convert Markdown to HTML
  * md_dita - convert Markdown to DITA
- * html_md - Convert HTML to Markdown""")
-    par.add_argument("-out", "--output", metavar="output_folder", help="manually specify the output folder (defaults to the input folder)")
+ * html_md - convert HTML to Markdown
+ * html_dita - convert HTML to DITA""")
+    par.add_argument("-out", "--output", metavar="output_dir", help="manually specify the output folder (defaults to the input folder)")
     args = par.parse_args()
 
     sys.tracebacklimit = 0 # Disables traceback messages
-    invalid_input = f" [!] {args.input} does not exist."
 
     # Set input and output folders
     if args.output:
-        output_folder = args.output
+        output_dir = args.output
     elif not args.output:
-        output_folder = args.input
+        output_dir = args.input
 
-    # Convert MD to...
     if args.convert == "md_html":
-        if os.path.isfile(args.input):
-            convert_file(args.input, "md", markdown_str_to_html_str, "html")
-        elif os.path.isdir(args.input):
-            convert_folder(args.input, "md", markdown_str_to_html_str, output_folder, "html")
-        else:
-            raise Exception(invalid_input)
+        md_html(args.input, output_dir)
     elif args.convert == "md_dita":
-        if os.path.isfile(args.input):
-            convert_file(args.input, "md", markdown_str_to_dita_str, "dita")
-        elif os.path.isdir(args.input):
-            convert_folder(args.input, "md", markdown_str_to_dita_str, output_folder, "dita")
-        else:
-            raise Exception(invalid_input)
-
-    # Convert HTML to...
+        md_dita(args.input, output_dir)
     elif args.convert == "html_dita":
-        if os.path.isfile(args.input):
-            convert_file(args.input, "html", html_str_to_dita_str, "dita")
-        elif os.path.isdir(args.input):
-            convert_folder(args.input, "html", html_str_to_dita_str, output_folder, "dita")
-        else:
-            raise Exception(invalid_input)
-    if args.convert == "html_md":
-        if os.path.isfile(args.input):
-            convert_file(args.input, "html", html_str_to_markdown_str, "md")
-        elif os.path.isdir(args.input):
-            convert_folder(args.input, "html", html_str_to_markdown_str, output_folder, "md")
-        else:
-            raise Exception(invalid_input)
+        html_dita(args.input, output_dir)
+    elif args.convert == "html_md":
+        html_md(args.input, output_dir)
 
 if __name__ == "__main__":
     main()
