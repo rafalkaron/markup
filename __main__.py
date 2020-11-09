3ccb100b290661f0db7b68368bbf111651d4de80
rafalkaron <rafalkaron@gmail.com>
2020-11-09 10:20:55 +0100

    code cleanup

--- __main__.py
+++ __main__.py
@@ -15,20 +15,19 @@ __version__ = "0.3"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 
 def main():
-    par = argparse.ArgumentParser(description="Batch-convert Markdown and HTML files to DITA.", formatter_class=argparse.RawTextHelpFormatter)
+    # sys.tracebacklimit = 0 # Disables traceback messages
+
+    par = argparse.ArgumentParser(description="Batch-convert Markdown and HTML files.", formatter_class=argparse.RawTextHelpFormatter)
     par.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
-    par.add_argument("input", type=str, help="provide a filepath to a file or a filder with files that you want to convert")
+    par.add_argument("input", type=str, help="provide a filepath to a file or a folder with files that you want to convert")
     par.add_argument("convert", type=str, help="""set the conversion type:
- * md_html - convert Markdown to HTML
  * md_dita - convert Markdown to DITA
- * html_md - convert HTML to Markdown
- * html_dita - convert HTML to DITA""")
+ * html_dita - convert HTML to DITA
+ * md_html - convert Markdown to HTML
+ * html_md - convert HTML to Markdown""")
     par.add_argument("-out", "--output", metavar="output_dir", help="manually specify the output folder (defaults to the input folder)")
     args = par.parse_args()
 
-    sys.tracebacklimit = 0 # Disables traceback messages
-
-    # Set input and output folders
     if args.output:
         output_dir = args.output
     elif not args.output:
@@ -42,6 +41,9 @@ def main():
         html_dita(args.input, output_dir)
     elif args.convert == "html_md":
         html_md(args.input, output_dir)
+    else:
+        print("Run MarkUP again and set the right conversion type.")
+        par.print_help()
 
 if __name__ == "__main__":
     main()
