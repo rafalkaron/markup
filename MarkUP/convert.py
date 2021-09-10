05b0edf660c8bad835da8552d1b348765035ef4a
Rafał Karoń <rafalkaron@gmail.com>
2021-09-10 23:17:32 +0200

    update

--- MarkUP/convert.py
+++ MarkUP/convert.py
@@ -72,9 +72,11 @@ class Source:
                     f" [?] Do you want to overwrite {output_filepath}? [y/n]: ")
 
             if self.conversion == "md_dita":
-                output_str = self.markdown_str_to_dita_str(source_file_str)
+                output_str = self.markdown_str_to_dita_str(
+                    source_file_str, output_filename)
             elif self.conversion == "html_dita":
-                output_str = self.html_str_to_dita_str(source_file_str)
+                output_str = self.html_str_to_dita_str(
+                    source_file_str, output_filename)
             elif self.conversion == "md_html":
                 output_str = self.markdown_str_to_html_str(source_file_str)
             elif self.conversion == "html_md":
@@ -85,7 +87,7 @@ class Source:
         return True
 
     @staticmethod
-    def markdown_str_to_dita_str(markdown_str) -> str:
+    def markdown_str_to_dita_str(markdown_str, output_filename) -> str:
         converter = markdown2dita.Markdown(title_level=4)
         dita_str = converter(markdown_str)
         dita_str = re.sub("id=\"enter-id-here\"",
@@ -94,15 +96,16 @@ class Source:
         dita_str = re.sub(">\n><", ">\n<", dita_str)
         dita_str = re.sub(r"<shortdesc>Enter the short description for this page here</shortdesc>",
                           "<shortdesc></shortdesc>", dita_str)    # Removes the hardcoded shortdesc
-        # dita_str = re.sub("<title>Enter the page title here</title>",
-        #                  f"<title>{output_file.replace('.dita', '')}</title>", dita_str)   # Replaces the title to match filename
+        dita_str = re.sub("<title>Enter the page title here</title>",
+                          f"<title>{output_filename.replace('.dita', '')}</title>", dita_str)   # Replaces the title to match filename
         dita_str = re.sub("\n\n", "\n", dita_str)
         return dita_str
 
     @staticmethod
-    def html_str_to_dita_str(html_str) -> str:
+    def html_str_to_dita_str(html_str, output_filename) -> str:
         markdown_str = Source.html_str_to_markdown_str(html_str)
-        dita_str = Source.markdown_str_to_dita_str(markdown_str)
+        dita_str = Source.markdown_str_to_dita_str(
+            markdown_str, output_filename)
         return dita_str
 
     @staticmethod
@@ -120,55 +123,3 @@ class Source:
 
     def pretty_print(self):
         pass
-
-    """
-    def convert_file(self, source, converter, output_extension):
-        output_file = os.path.basename(re.sub(
-            f".{self.source_extension}", f".{output_extension}", source, flags=re.IGNORECASE))
-        output_filepath = self.output_dir + "/" + output_file
-        if os.path.isfile(output_filepath):
-            boolean_prompt(
-                f" [?] Do you want to overwrite {output_filepath}? [y/n]: ")
-        start_time = time.time()
-        output_str = converter(read_file(source), output_file)
-        save_str_as_file(output_str, output_filepath)
-        elapsed_time = time.time() - start_time
-        print(
-            f" [i] Converted {source} to {output_filepath} in {round(elapsed_time, 3)} seconds.")
-        return output_filepath
-
-    def convert_folder(self, source, source_extension, converter, output_dir, output_extension):
-        elapsed_time = 0
-        input_files = files_list(source, source_extension)
-        files_number = len(input_files)
-        existing_files = []
-        if files_number == 0:
-            raise Exception(
-                f" [!] No {source_extension.upper()} files found in {source}")
-
-        for input_filepath in input_files:
-            output_file = os.path.basename(re.sub(
-                f".{source_extension}", f".{output_extension}", input_filepath, flags=re.IGNORECASE))
-            output_filepath = output_dir + "/" + output_file
-            if os.path.isfile(output_filepath):
-                existing_files.append(output_filepath)
-        existing_files_str = "\n".join(existing_files)
-        existing_files_str = "     * " + \
-            existing_files_str.replace("\n", "\n     * ")
-        if len(existing_files) > 0:
-            boolean_prompt(
-                f" [?] Do you want to overwrite the following files? [y/n]\n{existing_files_str}\n [>] ")
-        for input_filepath in input_files:
-            start_time = time.time()
-            output_file = os.path.basename(re.sub(
-                f".{source_extension}", f".{output_extension}", input_filepath, flags=re.IGNORECASE))
-            output_filepath = output_dir + "/" + output_file
-            output_str = converter(read_file(input_filepath), output_file)
-            save_str_as_file(output_str, output_filepath)
-            iter_time = time.time() - start_time
-            elapsed_time = elapsed_time + iter_time
-            print(f" [+] Converted {input_filepath} to {output_filepath}")
-        print(
-            f" [i] Converted {files_number} {source_extension.upper()} file(s) to {output_extension.upper()} in {round(elapsed_time, 3)} seconds.")
-        return True
-"""
