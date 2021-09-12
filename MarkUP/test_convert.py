9def056391c35b840b74fdcfb9f5278ceb39b609
Rafał Karoń <rafalkaron@gmail.com>
2021-09-12 20:07:33 +0200

    update

--- MarkUP/test_convert.py
+++ MarkUP/test_convert.py
@@ -1,71 +1,74 @@
 import unittest
 import mock
 import os
-import shutil
 from MarkUP import Source
 
 
-class TestConvert(unittest.TestCase):
+md_a_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/md/a.md"
+md_a_dita_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/md/a.dita"
+md_a_html_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/md/a.html"
 
-    def setUp(self):
-        self.md_a_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/md/a.md"
-        self.md_a_dita_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/md/a.dita"
-        self.md_a_html_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/md/a.html"
+html_a_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/html/a.html"
+html_a_dita_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/html/a.dita"
+html_a_md_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/html/a.md"
 
-        self.html_a_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/html/a.html"
-        self.html_a_dita_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/html/a.dita"
-        self.html_a_md_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/html/a.md"
+dita_a_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/dita/a.dita"
+dita_a_html_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/dita/a.dita"
+dita_a_md_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/dita/a.md"
 
-        self.dita_a_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/dita/a.dita"
-        self.dita_a_html_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/dita/a.dita"
-        self.dita_a_md_filepath = "/Users/rafalkaron/GitHub/MarkUP/tests/data/dita/a.md"
+md_dir = "/Users/rafalkaron/GitHub/MarkUP/tests/data/md"
+html_dir = "/Users/rafalkaron/GitHub/MarkUP/tests/data/html"
+dita_dir = "/Users/rafalkaron/GitHub/MarkUP/tests/data/dita"
 
-        self.md_dir = "/Users/rafalkaron/GitHub/MarkUP/tests/data/md"
-        self.html_dir = "/Users/rafalkaron/GitHub/MarkUP/tests/data/html"
-        self.dita_dir = "/Users/rafalkaron/GitHub/MarkUP/tests/data/dita"
+output_dir = "/Users/rafalkaron/GitHub/MarkUP/tests/data/out"
 
-        self.output_dir = "/Users/rafalkaron/GitHub/MarkUP/tests/data/out"
 
-    def test_md_to_dita_file(self):
+class TestConvertFile(unittest.TestCase):
+
+    def test_md_to_dita(self):
         try:
-            os.remove(self.md_a_filepath.replace(".md", ".dita"))
+            os.remove(md_a_filepath.replace(".md", ".dita"))
         except:
             pass
 
-        source = Source(self.md_a_filepath, "md_dita")
+        source = Source(md_a_filepath, "md_dita")
         with mock.patch('builtins.input', return_value="y"):
             output_filepath = source.convert()[0]
         self.assertTrue(os.path.isfile(output_filepath))
+        self.assertEqual(output_filepath, md_a_dita_filepath)
 
-    def test_md_to_html_file(self):
+    def test_md_to_html(self):
         try:
-            os.remove(self.md_a_filepath.replace(".md", ".html"))
+            os.remove(md_a_filepath.replace(".md", ".html"))
         except:
             pass
 
-        source = Source(self.md_a_filepath, "md_html")
+        source = Source(md_a_filepath, "md_html")
         with mock.patch('builtins.input', return_value="y"):
             output_filepath = source.convert()[0]
         self.assertTrue(os.path.isfile(output_filepath))
+        self.assertEqual(output_filepath, md_a_html_filepath)
 
-    def test_html_to_dita_file(self):
+    def test_html_to_dita(self):
         try:
-            os.remove(self.html_a_filepath.replace(".html", ".dita"))
+            os.remove(html_a_filepath.replace(".html", ".dita"))
         except:
             pass
 
-        source = Source(self.html_a_filepath, "html_dita")
+        source = Source(html_a_filepath, "html_dita")
         with mock.patch('builtins.input', return_value="y"):
             output_filepath = source.convert()[0]
         self.assertTrue(os.path.isfile(output_filepath))
+        self.assertEqual(output_filepath, html_a_dita_filepath)
 
-    def test_html_to_md_file(self):
+    def test_html_to_md(self):
         try:
-            os.remove(self.html_a_filepath.replace(".html", ".md"))
+            os.remove(html_a_filepath.replace(".html", ".md"))
         except:
             pass
 
-        source = Source(self.html_a_filepath, "html_md")
+        source = Source(html_a_filepath, "html_md")
         with mock.patch('builtins.input', return_value="y"):
             output_filepath = source.convert()[0]
         self.assertTrue(os.path.isfile(output_filepath))
+        self.assertEqual(output_filepath, html_a_md_filepath)
