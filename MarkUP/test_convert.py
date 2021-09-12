eb82091161ade34ba83d820dcade4f4bbd997510
Rafał Karoń <rafalkaron@gmail.com>
2021-09-12 07:27:08 +0200

    first test

--- MarkUP/test_convert.py
+++ MarkUP/test_convert.py
@@ -1,13 +1,14 @@
 import unittest
+import mock
 import os
 from MarkUP import Source
 
 
 class TestConvert(unittest.TestCase):
 
-    def test_md_dita_file(self):
+    def test_md_to_dita_file(self):
         source = Source(
-            "/Users/rafalkaron/GitHub/MarkUP/tests/data/README.md", "md_dita")
-        Source.output_file = source.convert()
-
-        self.assertTrue(os.path.isfile(Source.output_file))
+            "/Users/rafalkaron/GitHub/MarkUP/tests/data/README.md", "md_dita", "")
+        with mock.patch('builtins.input', return_value="y"):
+            output_file = source.convert()
+        self.assertTrue(os.path.isfile(output_file[0]))
